c53599832402b3bfadd803d5ef828437d04d60fb
Ben Frauenknecht <ge47pos@mytum.de>
2023-04-13 12:50:18 +0200

    Auth added with refresh Option and IO persistence layer

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from spotify_auth import accessToken
+import json
+import os
 
-token = accessToken()
-print(token)
+from spotify_auth import spotifyTokens
+import requests
+from os.path import exists
+
+spotifyApiBaseUrl = "https://api.spotify.com/v1"
+tokens = spotifyTokens()
+
+with requests.Session() as session:
+    rep = session.get(f"{spotifyApiBaseUrl}/me/playlists", headers={"Authorization": f"Bearer {tokens['access_token']}"})
+    print(json.loads(rep.text))
--- spotify_auth.py
+++ spotify_auth.py
@@ -4,6 +4,7 @@ import secrets
 import hashlib
 import base64
 import string
+from os.path import exists
 
 from queue import Queue
 import threading
@@ -65,11 +66,15 @@ def fetch_Token(code):
              "code_verifier": codeVerifier}
     with requests.Session() as session:
         rep = session.post(tokenUrl, headers={"Content-Type": "application/x-www-form-urlencoded"}, data=param)
-        rep = json.loads(rep.text)
-        return rep["access_token"]
+        return rep.text
 
 
-def accessToken():
+def extractTokensFromResponse(httpRep):
+    js = json.loads(httpRep)
+    return {"access_token": js["access_token"], "refresh_token": js["refresh_token"]}
+
+
+def newSpotifyTokens():
     server = socketserver.TCPServer(("localhost", 8080), Handler)
     serverThread = threading.Thread(target=server.serve_forever)
     serverThread.daemon = True
@@ -81,11 +86,57 @@ def accessToken():
     server.shutdown()
     serverThread.join()
 
-    return fetch_Token(codeQueue.get())
+    rep = fetch_Token(codeQueue.get())
+    return extractTokensFromResponse(rep)
+
+
+def refreshAccessToken(refreshToken):
+    header = {"Content-Type": "application/x-www-form-urlencoded"}
+    body = {"grant_type": "refresh_token", "refresh_token": refreshToken, "client_id": clientId}
+    with requests.Session() as session:
+        rep = session.post("https://accounts.spotify.com/api/token", headers=header, data=body)
+    if rep.status_code != 200:
+        tokens = newSpotifyTokens()
+    else:
+        tokens = extractTokensFromResponse(rep.text)
+    tokenToIO(tokens)
+    return tokens
+
+
+def checkTokenValidity(accessToken):
+    with requests.Session() as session:
+        rep = session.get("https://api.spotify.com/v1/me", headers={"Authorization": f"Bearer {accessToken}"})
+    if rep.status_code != 200:
+        return False
+    return True
+
+
+def tokenToIO(tokens):
+    tokenFile = open("token.bin", "w")
+    tokenFile.write(f"{tokens['access_token']}\n")
+    tokenFile.write(tokens["refresh_token"])
+
+
+def tokenFromIO():
+    if not exists("token.bin"):
+        tokens = newSpotifyTokens()
+        tokenToIO(tokens)
+        return tokens
+
+    tokenFile = open("token.bin", "r")
+    tokens = tokenFile.readlines()
+    tokens = {"access_token": tokens[0].replace("\n", ""), "refresh_token": tokens[1]}
+    if checkTokenValidity(tokens["access_token"]):
+        return tokens
+    return refreshAccessToken(tokens["refresh_token"])
+
+
+def spotifyTokens():  # import this function
+    return tokenFromIO()
 
 
 def main():
-    print(f"Access Token: {accessToken()}")
+    print(f"Tokens: {spotifyTokens()}")
 
 
 if __name__ == "__main__":
